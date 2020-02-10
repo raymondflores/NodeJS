0948bb8550f0510434a1a26e47258aed1da6ba98
Raymond Flores <raymond@raymonds-mbp.lan>
2020-02-09 23:39:33 -0600

    node without express example

---
+++ node-without-express/routes.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+
+const requestHandler = (req, res) => {
+  const { url, method } = req;
+
+  if (url === '/') {
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title></head>');
+    res.write('<body><form action="/message" method="POST">');
+    res.write('<input type="text" name="message"/> <button type="submit">Send</button>');
+    res.write('</form></body>');
+    res.write('</html>');
+    return res.end();
+  }
+
+  if (url === '/message' && method === 'POST') {
+    const body = [];
+    req.on('data', chunk => {
+      body.push(chunk);
+    });
+
+    return req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.writeFile('./node-without-express/message.txt', message, err => {
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+      });
+    });
+  }
+
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My App</title></head>');
+  res.write('<body><h1>Hello World</h1></body>');
+  res.write('</html>');
+};
+
+module.exports = requestHandler;
